172f747038b5291f7f3ad339e6c4ec645a755e8f
Nathan Hillyer <nathanielhillyer@gmail.com>
2020-06-17 21:16:37 -0500

    Switch pip module to GPL 3. Closes #17

--- module/setup.py
+++ module/setup.py
@@ -11,11 +11,11 @@ setuptools.setup(
     description="CoLliage Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Toth-Technology/rad-i",
+    url="https://github.com/Toth-Technology/collageradiomics",
     py_modules=["collageradiomics"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     install_requires=[
