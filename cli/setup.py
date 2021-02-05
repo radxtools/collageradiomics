1ba11db38654cca2e9a6f741adb278a1faaa6879
sarthakpati <sarthak.pati@hotmail.com>
2021-02-05 07:50:01 -0500

    ensuring version is picked up properly

--- cli/setup.py
+++ cli/setup.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('../module/README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('../module/VERSION') as version_file:
+    version = version_file.read().strip()
 
 setup(name='collageradiomicscli',
-      version='1.0.0',
+      version=version,      
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       description='Get Collage features from an image and a binary mask',
       url='https://github.com/radxtools/collageradiomics',
       python_requires='>=3.6',
       author='Toth Technology',
       author_email='toth-tech@hillyer.me',
       license='BSD-3-Clause',
+      packages=find_packages(),
       zip_safe=False,
       install_requires=[
-      'collageradiomics',
+      'collageradiomics=='+version,
       'setuptools>=47',
       'SimpleITK==1.2.4',
       'click',
